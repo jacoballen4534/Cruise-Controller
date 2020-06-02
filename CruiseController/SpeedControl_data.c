f2a314a98f025981c58ae307d1c6d8e2d0c06f17
jacoballen4534 <jacoballen4534@gmail.com>
2020-06-02 17:50:30 +1200

    Add Speed control esterel and c

--- CruiseController/SpeedControl_data.c
+++ CruiseController/SpeedControl_data.c
@@ -6,18 +6,21 @@ PARAMETERS: throttleIn - throttle input
             saturate - true if saturated, false otherwise
 RETURNS: throttle output (ThrottleCmd)
 */
-float saturateThrottle(float throttleIn, bool* saturate)
+float saturateThrottle(float throttleIn, bool *saturate)
 {
 	static const float THROTTLESATMAX = 45.0;
-	if (throttleIn > THROTTLESATMAX) {
+	if (throttleIn > THROTTLESATMAX)
+	{
 		*saturate = true;
 		return THROTTLESATMAX;
 	}
-	else if (throttleIn < 0) {
+	else if (throttleIn < 0)
+	{
 		*saturate = true;
 		return 0;
 	}
-	else {
+	else
+	{
 		*saturate = false;
 		return throttleIn;
 	}
@@ -36,15 +39,16 @@ float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed)
 	static const float KI = 0.5;
 	static bool saturate = true;
 	static float iterm = 0;
-	
-	if (isGoingOn) {
-		iterm = 0;	// reset the integral action
-		saturate = true;	
+
+	if (isGoingOn)
+	{
+		iterm = 0; // reset the integral action
+		saturate = true;
 	}
 	float error = cruiseSpeed - vehicleSpeed;
 	float proportionalAction = error * KP;
 	if (saturate)
-		error = 0;	// integral action is hold when command is saturated
+		error = 0; // integral action is hold when command is saturated
 	iterm = iterm + error;
 	float integralAction = KI * iterm;
 	return saturateThrottle(proportionalAction + integralAction, &saturate);
